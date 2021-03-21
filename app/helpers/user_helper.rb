756749f688e89d99bde5d10546b3685de6989e01
necmigunduz <necmigunduz@gmail.com>
2021-03-22 01:13:27 +0300

    Fix queries for followed by and who to follow parts

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,6 +4,11 @@ module UserHelper
         []
     end
 
+    def by_followeds
+        return current_user.by_followeds if current_user
+        []
+    end
+
     def opinions
         @opinions = current_user.opinions.ordered_by_most_recent
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,23 +16,42 @@ class User < ApplicationRecord
         following = Following.select(:followed_id).where(follower_id: self.id)
         # Get who are the followers from current user
         followers = Following.select(:follower_id).where(followed_id: self.id)
-        
         # Creates a friendship array with all relationships the current user has
         friendship = []
 
         following.map do |f|
             friendship.push(f.followed_id)
         end
+
         followers.map do |f|
             friendship.push(f.follower_id)
         end
+        
         friendship.push(self.id)
-
 
         User.where.not(id: friendship)
         # This will produce SQL query with IN. Something like: select * from posts where user_id NOT IN (1,45,874,43);
     end
 
+    def by_followeds
+        # Get who the current user are following
+        # following = Following.select(:followed_id).where(follower_id: self.id)
+        # # Get who are the followers from current user
+        followers = Following.select(:follower_id).where(followed_id: self.id)
+        # Creates a friendship array with all relationships the current user has
+        friendship = []
+
+        # following.map do |f|
+        #     friendship.push(f.followed_id)
+        # end
+
+        followers.map do |f|
+            friendship.push(f.follower_id)
+        end
+        
+        User.where(id: friendship)
+        # This will produce SQL query with IN. Something like: select * from posts where user_id NOT IN (1,45,874,43);
+    end
 
     def friends_and_own_posts
         Opinion.where(author_id: (follows.to_a << self))
